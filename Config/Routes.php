8afe23efe6e56216f70a5418f9a20abe46a34792
Alatise Oluwaseun <holynation667@gmail.com>
2022-01-04 23:58:28 +0100

    modify the routes to groups and single

--- Config/Routes.php
+++ Config/Routes.php
@@ -28,10 +28,57 @@ $routes->setAutoRoute(false);
  * Route Definitions
  * --------------------------------------------------------------------
  */
-
+ 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Api::index');
+
+// The route for the application mobile API is here
+$routes->group('api',['filter'=>'apiAuth'], function($routes){
+    $routes->add('(:any)','Api::mobileApi/$1');
+    $routes->add('(:any)/(:any)','Api::mobileApi/$1/$2');
+    $routes->add('(:any)/(:any)','Api::mobileApi/$1/$2/$3');
+});
+
+// The route for normal operations for the backend
+$routes->group('vc/create',function($routes){
+    $routes->add('(:any)', 'ViewController::create/$1');
+    $routes->add('(:any)/(:any)', 'ViewController::create/$1/$2');
+    $routes->add('(:any)/(:any)/(:any)', 'ViewController::create/$1/$2/$3');
+    $routes->add('(:any)/(:any)/(:any)/(:any)', 'ViewController::create/$1/$2/$3/$4');
+});
+
+$routes->group('vc', function($routes){
+    $routes->add('resetPassword/(:any)','ViewController::resetPassword/$1');
+    $routes->add('changePassword','ViewController::changePassword');
+    $routes->add('changePassword/(:any)','ViewController::changePassword/$1');
+    $routes->add('(:any)','ViewController::view/$1');
+    $routes->add('(:any)/(:any)','ViewController::view/$1/$2');
+    $routes->add('(:any)/(:any)/(:any)','ViewController::view/$1/$2/$3');
+});
+
+$routes->group('mc', function($routes){
+    $routes->add('(:any)','ModelController::view/$1');
+    $routes->add('(:any)/(:any)','ModelController::view/$1/$2');
+    $routes->add('(:any)/(:any)/(:any)','ModelController::view/$1/$2/$3');
+    $routes->add('(:any)/(:any)/(:any)/(:any)','ModelController::view/$1/$2/$3/$4');
+    $routes->add('(:any)/(:any)/(:any)/(:any)/(:any)','ModelController::view/$1/$2/$3/$4/$5');
+});
+
+$routes->group('ac', function($routes){
+    $routes->add('(:any)','ActionController/$1');
+    $routes->add('(:any)/(:any)','ActionController/$1/$2');
+    $routes->add('(:any)/(:any)/(:any)','ActionController/$1/$2/$3');
+});
+
+$routes->add('delete/(:any)/(:any)','ActionController::delete/$1/$2');
+$routes->add('delete/(:any)/(:any)/(:any)','ActionController::delete/$1/$2/$3');
+$routes->add('truncate/(:any)','ActionController::truncate/$1');
+
+$routes->add('edit/(:any)/(:any)','ActionController::edit/$1/$2');
+
+
+
 
 /*
  * --------------------------------------------------------------------
